46005f37858457ab97a9a0eb26dbe358a415d713
alephlichti-png <aleph.lichti@escola.pr.gov.br>
2025-09-05 12:12:29 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,5 +1,6 @@
 criaCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’
+    ‘Programação’,
+    ‘O que é java script’,
+    ‘O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web,
+permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.’
 )
