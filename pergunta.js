99ca266d31051829a7f243fbe578e516edf8c0cf
alephlichti-png <aleph.lichti@escola.pr.gov.br>
2025-09-05 12:19:59 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,17 +1,17 @@
 criaCartao(
     ‘Programação’,
-    ‘O que é JavaScript’,
+    ‘O que é JavaScript?’,
     ‘O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web,
 permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.’
 )
 criaCartao(
     ‘Programação’,
-    ‘O que é CSS’,
+    ‘O que é CSS?’,
     ‘resposta’
 )
 criaCartao(
-    ‘categoria’,
-    ‘pergunta’,
+    ‘Geografia’,
+    ‘Em que ano o Brasil foi descoberto?’,
     ‘resposta’
 )
 criaCartao(
