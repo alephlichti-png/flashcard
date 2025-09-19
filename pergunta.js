01a181691e79cdd974177d4bdee00ec3a29af17e
alephlichti-png <aleph.lichti@escola.pr.gov.br>
2025-09-19 12:02:28 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,33 +1,30 @@
 criaCartao(
-    ‘Programação’,
-    ‘O que é JavaScript?’,
-    ‘O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web,
-permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.’
+    'Programação’,
+    'O que é JavaScript?’,
+    'O JavaScript é uma linguagem de programação usada para criar interatividade em páginas web, permitindo que elas respondam a ações do usuário, como cliques ou movimentos do mouse.'
 )
 criaCartao(
-    ‘Programação’,
-    ‘O que é CSS?’,
-    ‘O CSS é uma linguagem de estilização, permitindo aplicar estilos diretamente nos elementos
-HTML, alterando sua aparência visual.’
+    'Programação’,
+    'O que é CSS?’,
+    'O CSS é uma linguagem de estilização, permitindo aplicar estilos diretamente nos elementos HTML, alterando sua aparência visual.'
 )
 criaCartao(
-    ‘Geografia’,
-    ‘Em que ano o Brasil foi descoberto?’,
-    ‘resposta’
+    'Geografia’,
+    'Em que ano o Brasil foi descoberto?’,
+    'resposta'
 )
 criaCartao(
-    ‘Geografia’,
-    ‘Onde se localiza o Peru?’,
-    ‘No continente americano sul’
+    'Geografia’,
+    'Onde se localiza o Peru?’,
+    'No continente americano sul'
 )
 criaCartao(
-    ‘Inglês’,
-    ‘O que é verbo "to be"?’,
-    ‘O verbo "to be" em inglês é um dos verbos mais fundamentais da língua, correspondendo aos verbos "ser" e "estar" em português.
-Ele é usado para descrever a condição, estado, identidade ou localização de algo ou alguém.’
+    'Inglês’,
+    'O que é verbo "to be"?’,
+    'O verbo "to be" em inglês é um dos verbos mais fundamentais da língua, correspondendo aos verbos "ser" e "estar" em português. Ele é usado para descrever a condição, estado, identidade ou localização de algo ou alguém.'
 )
 criaCartao(
-    ‘Geografia’,
-    ‘Onde está localizado o Estados Unidos’,
-    ‘Se localiza no continente americano Norte’
+    'Geografia’,
+    'Onde está localizado o Estados Unidos’,
+    'Se localiza no continente americano Norte'
 )
